7dab69496e88d6dffda9856f1cd5619dc3fabf58
ifdevelop <ifdevelop@users.noreply.github.com>
2020-02-07 21:36:19 +0300

    Created chat message bubbles

---
+++ Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatMessageListControl.xaml
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatMessageListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
